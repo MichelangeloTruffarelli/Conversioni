0b3cfb3940341aaa67e181c807b3ea5b27f14eaf
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-20 12:39:36 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 0;
+            value = Convert.ToInt32(Console.ReadLine());
+            string binary = Convert.ToString(value, 2);
+            Console.WriteLine(binary);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Conversioni/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Conversioni
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string n = TxtNumero.Text;
+            if (cmb.SelectedIndex == 0)
+            {
+                int ris = Convert.ToInt32(n, 2);
+                TxtConvert.Text = ris.ToString();
+            }
+            else if ( cmb.SelectedIndex == 1)
+            {
+                int a = int.Parse(n);
+                string ris = Convert.ToString(a, 2);
+                TxtConvert.Text = ris.ToString();
+            }
+        }
+    }
+}
